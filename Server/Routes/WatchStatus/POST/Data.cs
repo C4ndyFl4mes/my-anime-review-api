ae25064ef7b427208d593591d07c81ee8e4a9e4b
Isaac Svedin <swedinisaac@gmail.com>
2026-07-05 19:55:50 +0200

    Changed the post watch status endpoint to replace rather than just adding. The Put endpoint may be removed later.

--- Server/Routes/WatchStatus/POST/Data.cs
+++ Server/Routes/WatchStatus/POST/Data.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Server.Data;
 using Server.Entities;
 using Server.Exceptions;
@@ -9,11 +10,15 @@ public class PostWatchStatusData(AppDbContext ctx)
 {
     public async Task<WatchStatusResponse> PostWatchStatusAsync(int animeId, Guid currentUserId, PostWatchStatusRequest request, CancellationToken ct)
     {
+        IDbContextTransaction? transaction = await ctx.Database.BeginTransactionAsync(ct);
+
         AnimeEntity anime = await ctx.Animes.FirstOrDefaultAsync(a => a.Id == animeId, ct) ??
             throw new NotFoundException("The anime doesn't exist.");
 
         if (anime.TotalEpisodes is not null && request.EpisodesWatched > anime.TotalEpisodes)
             throw new BadRequestException("You can't watch more episodes than the anime has total episodes.");
+        
+        ctx.Remove(anime); // Removes the anime from the database to avoid duplicate entries, as it will be re-added with the new watch status.
         
         WatchStatusEntity status = new()
         {
@@ -30,6 +35,8 @@ public class PostWatchStatusData(AppDbContext ctx)
         await ctx.WatchStatuses.AddAsync(status, ct);
 
         await ctx.SaveChangesAsync(ct);
+
+        await transaction.CommitAsync(ct);
 
         return new()
         {
