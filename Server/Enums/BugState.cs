2700ac7db4d10b4784cdd98ab09086e6842321e3
C4ndyFl4mes <swedinisaac@gmail.com>
2026-07-15 16:49:07 +0200

    Fixed BugState enum to be able to automatically convert to or from string.

--- Server/Enums/BugState.cs
+++ Server/Enums/BugState.cs
@@ -1,10 +1,18 @@
+using System.Text.Json.Serialization;
+
 namespace Server.Enums;
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum BugState
 {
+    [JsonStringEnumMemberName("Pending")]
     Pending,
+    [JsonStringEnumMemberName("Planned")]
     Planned,
+    [JsonStringEnumMemberName("In Progress")]
     InProgress,
+    [JsonStringEnumMemberName("Completed")]
     Completed,
+    [JsonStringEnumMemberName("Rejected")]
     Rejected
 }
