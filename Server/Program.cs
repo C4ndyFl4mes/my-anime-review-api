cb4f1fc742d35d7f1645fc828aad67b5e46bf046
Isaac Svedin <swedinisaac@gmail.com>
2026-07-22 13:37:11 +0200

    Added domains in cors origins.

--- Server/Program.cs
+++ Server/Program.cs
@@ -93,7 +93,9 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5173",
-                "https://localhost:5173"
+                "https://localhost:5173",
+                "https://myanimereview.se/",
+                "https://www.myanimereview.se/"
               )
               .AllowAnyHeader()
               .AllowAnyMethod()
